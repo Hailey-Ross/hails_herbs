2bbc34cf9fa7c17a8587f171fc9bced5ae9ced94
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-03-07 00:18:49 -0700

    Initial Commit

---
+++ items.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_acorn', 'Acorn', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_herb_evergreen_huckleberry', 'Evergreen Huckleberies', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_peach', 'Peach', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_pear', 'Pear', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('apple', 'Apple', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('carrots', 'Carrots', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('blueberry', 'Blueberries', 20, 1, 'item_standard', 1);
