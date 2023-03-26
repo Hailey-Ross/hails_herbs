67a2dbfaaefe712509eb22c9d3938b6367b881de
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-03-26 03:01:09 -0600

    Version Control Addition

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,3 +10,8 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 client_script 'client/client.lua'
 shared_script 'config.lua'
 server_script 'server/server.lua'
+
+version '0.1'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/Hailey-Ross/hails_herbs'
