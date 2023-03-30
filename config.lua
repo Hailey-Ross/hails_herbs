9e0803de919b58d205162a0156c64e6e47c6cfe6
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-03-30 14:26:32 -0600

    config commenting

--- config.lua
+++ config.lua
@@ -4,6 +4,29 @@ Config.debug = false
 
 Config.cooldowntimer = 5000
 
+--[[
+
+    Colors:
+        Red          ~e~
+        Yellow       ~o~
+        Orange       ~d~
+        Gray         ~m~
+        White        ~q~
+        Light Gray   ~t~
+        Black        ~v~
+        Pink         ~u~
+        Blue         ~pa~
+        Purple       ~t1~
+        Orange?      ~t2~
+        Light Blue   ~t3~
+        Yellow?      ~t4~
+        Light Pink   ~t5~
+        Green        ~t6~
+        Dark Blue    ~t7~
+        Light Red    ~t8~
+
+]]
+
 Config.namecolor = '~t6~'
 Config.alertcolor = '~q~'
 
